e77705966cb6cd3e467e53543d540c623372d893
thefarismd <thefarismd@icloud.com>
2023-02-07 17:34:54 +0800

    .env environmental variables

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //jshint esversion:6
 
 //---- Modules ----//
+require('dotenv').config() //Always at the top
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,8 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema); //
 
