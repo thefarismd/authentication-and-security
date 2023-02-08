9f4ed5acd73e2d803e8c15d54c783bf02b896f4b
thefarismd <thefarismd@icloud.com>
2023-02-08 15:54:29 +0800

    Level 5 - Cookies and Sessions

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//passport, passport-local, passport-local-mongoose, express-session
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMoongoose = require("passport-local-mongoose");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -17,17 +19,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); //
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //---- MongoDB-Moongoose Connection DB, Schema, Model ----//
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
 const userSchema = new mongoose.Schema({
-    email: String,
+    username: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMoongoose); //salt & hash variables to moongodb
+
 const User = new mongoose.model('User', userSchema); //
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //---- Get Route ----//
 app.get("/", function (req, res) {
@@ -42,45 +59,52 @@ app.get("/register", function (req, res) {
     res.render('register');
 });
 
+app.get("/secrets", function (req, res) {
+
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req,res){
+    req.logout(function(err){
+        if(!err){
+            res.redirect('/');
+        }
+    });
+});
+
 //---- Post Route ----//
 app.post("/register", function (req, res) {
-
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-
-        newUser.save(function (err) {
-            if (!err) {
-                res.render("secrets");
-            } else {
-                console.log(err);
-            };
-        });
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        };
     });
-
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({ email: username }, function (err, foundUser) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                // Load hash from your password DB.
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    // result == true
-                    if (result === true){
-                        res.render("secrets");
-                    };
-                });
-            };
-        };
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
     });
 });
 
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
 # Tech Stack(s)
+## Libraries
 - dotenv (environment variables)
 - express (express module)
 - mongoose (mongodb library)
 - md5 (password hashing)
 - body-parser (html content to app.js)
 - ejs (templating)
-- moongoose-encryption (database encryption)
+- moongoose-encryption.js (database encryption)
 - bcrypt (salting and hashing encryption)
+- passport, passport-local, passport-local-mongoose, express-session
+## Database
+- mongoDB
+## Backend
+- node.js
